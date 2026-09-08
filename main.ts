720ad1689590b45b87f676348f2e12aa1ef40e7f
sky <sasakura.michiko@gmail.com>
2026-09-08 17:44:50 +0900

    Replace infrared remote calls with rc_tablelight

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 input.onButtonPressed(Button.A, function () {
-    赤外線リモコン.ライトを点ける()
+    rc_tablelight.ライトを点ける()
 })
+
 input.onButtonPressed(Button.B, function () {
-    赤外線リモコン.ライトを消す()
+    rc_tablelight.ライトを消す()
 })
